38dbccc54679711c3042d8b7ed39d27f4a233324
Monish Sai <vikram.saithota.s86@kalvium.community>
2025-02-04 17:37:39 +0530

    frontend

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content:["./src/**/*.{html,js,jsx}"],
+  mode:"jit",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
